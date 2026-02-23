30b17a6fb30288275cfd1947c95aca3357dd84ca
mydr3z <ne91603@gmail.com>
2026-02-23 11:43:30 +0300

    refactor(FirstPersonController): renaming of variables

--- Scripts/PlayerStateMachine.gd
+++ Scripts/PlayerStateMachine.gd
@@ -2,19 +2,19 @@ extends Node
 
 class_name PlayerStateMachine
 
-var _current_state:PlayerState
+var _currentState:PlayerState
 
 
 func initialize(startingState:PlayerState):
-	_current_state = startingState;
+	_currentState = startingState;
 	startingState.enter();
 
 
 func change_state(newState:PlayerState):
-	_current_state.exit();
-	_current_state = newState;
+	_currentState.exit();
+	_currentState = newState;
 	newState.enter();
 
 
 func get_state()->PlayerState:
-	return _current_state;
+	return _currentState;

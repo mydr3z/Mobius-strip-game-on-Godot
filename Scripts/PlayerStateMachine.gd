2fb17f05443b23e816cec55b822fae955d00d889
mydr3z <ne91603@gmail.com>
2026-02-23 11:52:01 +0300

    refactor(PlayerStateMachine): rename of variables

--- Scripts/PlayerStateMachine.gd
+++ Scripts/PlayerStateMachine.gd
@@ -2,19 +2,19 @@ extends Node
 
 class_name PlayerStateMachine
 
-var _currentState:PlayerState
+var _current_state:PlayerState
 
 
-func initialize(startingState:PlayerState):
-	_currentState = startingState;
+func initialize(startingState:PlayerState)->void:
+	_current_state = startingState;
 	startingState.enter();
 
 
-func change_state(newState:PlayerState):
-	_currentState.exit();
-	_currentState = newState;
-	newState.enter();
+func change_state(_new_state:PlayerState)->void:
+	_current_state.exit();
+	_current_state = _new_state;
+	_new_state.enter();
 
 
 func get_state()->PlayerState:
-	return _currentState;
+	return _current_state;
